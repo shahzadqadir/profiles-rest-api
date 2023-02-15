326c7f49420d4008bd94ca412e24a1fe4eb87f26
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2023-02-15 12:09:16 +0000

    added post,put methods to our API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        api_view = [
+        'list item 1',
+        'list item 2',
+        'list item 3'
+        ]
+        return Response({'api_view': api_view})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
